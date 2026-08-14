5415deec29f76f391631f7e58395951df2072e40
Uttam Maji <umaji@localhost.localdomain>
2026-08-14 10:10:22 +0530

    stack  is stl

--- first.cpp
+++ first.cpp
@@ -155,5 +155,29 @@ void explainDeque() {
 
 
 
+void explainStack() {
+    stack<int> st;
+
+    st.push(1);    // {1}
+    st.push(2);    // {2, 1}
+    st.push(3);    // {3, 2, 1}
+    st.push(4);    // {4, 3, 2, 1}
+    st.emplace(5); // {5, 4, 3, 2, 1}
+
+    cout << st.top(); // prints 5
+    // st[2] is invalid
+
+    st.pop(); // st looks like {4, 3, 2, 1}
+
+    cout << st.top();  // 4
+    cout << st.size(); // 4
+    cout << st.empty();
+
+    stack<int> st1, st2;
+    st1.swap(st2);
+}
+
+
+
     
 
