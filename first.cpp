475249a76d9b5978ef89b6f145b3d98ffe0647ce
Uttam Maji <umaji@localhost.localdomain>
2026-08-14 10:04:21 +0530

    List is stl

--- first.cpp
+++ first.cpp
@@ -121,5 +121,19 @@ void explainVector()
 
 
 void explainList(){
-    
+    list<int> ls;
+    ls.push_back(2);//{2}
+    ls.emplace_back(4);//{2,4}
+
+    ls.push_front(5); //{5,2,4}
+
+    ls.emplace_front();//{2,4}
+
+    //rest functions same as  vector 
+    //begin , end. rbegin, ren,  clear, insert, size, swap
 }
+
+
+
+    
+
