1e48a9ca1dc6f5fe2f316905edc060dd6d1d8ac4
Uttam Maji <umaji@localhost.localdomain>
2026-08-14 10:06:18 +0530

    Deque is stl

--- first.cpp
+++ first.cpp
@@ -133,6 +133,26 @@ void explainList(){
     //begin , end. rbegin, ren,  clear, insert, size, swap
 }
 
+void explainDeque() {
+    deque<int> dq;
+
+    dq.push_back(1);     // [1]
+    dq.push_back(2);     // [1, 2]
+
+    dq.push_front(4);    // [4, 1, 2]
+    dq.push_front(3);    // [3, 4, 1, 2]
+
+    dq.pop_back();       // [3, 4, 1]
+    dq.pop_front();      // [4, 1]
+
+    cout << dq.back();   // 1
+    cout << dq.front();  // 4
+
+    // Other functions are similar to vector:
+    // begin(), end(), rbegin(), rend()
+    // clear(), insert(), size(), swap()
+}
+
 
 
     
