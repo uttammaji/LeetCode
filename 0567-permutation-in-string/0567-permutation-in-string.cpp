a576e3a613275740cb11148f72d136a751fd37ad
Uttam Maji <uttammaji842@gmail.com>
2026-09-25 16:40:01 +0530

    Time: 203 ms (19.24%), Space: 10.1 MB (46.15%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+
+    bool isFrequency(int freq1[],int freq2[]){//O(1)
+        for(int i=0;i<26;i++){
+            if(freq1[i]!=freq2[i]){
+                return false;
+            }
+        }
+        return true;
+
+    }
+    bool checkInclusion(string s1, string s2) {
+        int freq[26]={0};
+        for(int i=0;i<s1.length();i++){
+            freq[s1[i]-'a']++;
+
+        }
+
+        int windSize=s1.length();
+        for(int i=0;i<s2.length();i++){
+            int windIdx=0,idx=i;
+            int windFreq[26]={0};
+            while(windIdx<windSize && idx<s2.length()){
+                windFreq[s2[idx]-'a']++;
+                windIdx++;idx++;
+            }
+            if(isFrequency(freq,windFreq)){//found
+                return true;
+            }
+
+        }
+        return false;
+    }
+};
